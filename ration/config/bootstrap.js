e23caf532a08a91d198ce78f9a9d3289d9c2542b
sami peralahti <sami.peralahti@vainu.io>
2019-07-18 16:12:37 +0300

    same pw for both users

--- ration/config/bootstrap.js
+++ ration/config/bootstrap.js
@@ -15,7 +15,7 @@ module.exports.bootstrap = async function() {
   var path = require('path');
 
   // This bootstrap version indicates what version of fake data we're dealing with here.
-  var HARD_CODED_DATA_VERSION = 0.4;
+  var HARD_CODED_DATA_VERSION = 0.5;
 
   // This path indicates where to store/look for the JSON file that tracks the "last run bootstrap info"
   // locally on this development computer (if we happen to be on a development computer).
@@ -64,7 +64,7 @@ module.exports.bootstrap = async function() {
           emailAddress: 'sami@example.com',
           fullName: 'Kappa King',
           isSuperAdmin: true,
-          password: await sails.helpers.passwords.hashPassword('123')
+          password: await sails.helpers.passwords.hashPassword('abc123')
       },
       {
           emailAddress: 'admin@example.com',
