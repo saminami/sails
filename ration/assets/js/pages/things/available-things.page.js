ea37a56032960ab2aedfe633976fcf26b440d067
sami peralahti <sami.peralahti@vainu.io>
2019-07-21 19:35:26 +0300

    setup for the ajax form

--- ration/assets/js/pages/things/available-things.page.js
+++ ration/assets/js/pages/things/available-things.page.js
@@ -1,40 +1,48 @@
 parasails.registerPage('available-things', {
-  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
-  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
-  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
-  data: {
-    things:[],
-    confirmDeleteModalOpen: false,
-  },
-
-  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
-  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
-  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
-    // Attach any initial data from the server.
-    _.extend(this, SAILS_LOCALS);
-    
-  },
-  mounted: async function() {
-    //…
-  },
-
-  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
-  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-  methods: {
-    clickThing: async function(thingId) {
-      const thingToModify = this.things.find(item => item.id === thingId);
-      Object.assign(thingToModify, { label: 'Removing' });  
-      this.$forceUpdate();
-      await Cloud.destroyOneThing.with({ id: thingId });
-      _.remove(this.things, { id: thingId });
-      this.$forceUpdate();
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: {
+        things: [],
+        confirmDeleteModalOpen: false,
+        selectedThing: null,
+        // syncing / loading state
+        syncing: false,
+        // server Error state
+        cloudError: "",
     },
-    clickDeleteButton: async function(thingId) {
-      console.log("Clicked delete")
-      this.confirmDeleteModalOpen = true;
-    }
 
-  }
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+        // Attach any initial data from the server.
+        _.extend(this, SAILS_LOCALS);
+    },
+    mounted: async function() {
+        //…
+    },
+
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+        // clickThing: async function(thingId) {
+        //     const thingToModify = this.things.find(item => item.id === thingId);
+        //     Object.assign(thingToModify, { label: 'Removing' });
+        //     this.$forceUpdate();
+        //     await Cloud.destroyOneThing.with({ id: thingId });
+        //     _.remove(this.things, { id: thingId });
+        //     this.$forceUpdate();
+        // },
+        clickDeleteButton: function(thingId) {
+            this.selectedThing = _.find(this.things, { id: thingId });
+            this.confirmDeleteModalOpen = true;
+        },
+
+        closeDeleteModal: function(thingId) {
+            this.selectedThing = null;
+            this.confirmDeleteModalOpen = false;
+        }
+    }
 });
